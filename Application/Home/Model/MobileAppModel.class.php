7a22258940a38f75e46f78574ba07d501d5ee865
Andrii Palchik <palchik00@gmail.com>
2020-01-08 14:15:13 +0200

    final cleanings of code

--- Application/Home/Model/MobileAppModel.class.php
+++ Application/Home/Model/MobileAppModel.class.php
@@ -6,29 +6,34 @@ use Think\Model;
 
 class MobileAppModel extends Model
 {
+    
     protected $tableName = 'apps';
-    protected $_validate = array(
-        array('active', array(0,1), '', 3, 'in'),
+    protected $_validate = [
+        ['active', [0, 1], '', 3, 'in'],
         //@see https://github.com/CieloSun/StudentManageSystem/blob/master/Application/Sms/Model/TeacherModel.class.php
-    );
+    ];
     
-    protected $_auto = array(
-        array('active', 'setActive', 1, 'callback'),
-    );
-    
-    function setActive(){
-        if(I('post.active') == 'on'){
-            return 1;
-        }
-        return 0;
-    }
+    protected $_auto = [
+        ['active', 'setActive', 1, 'callback'],
+    ];
     
     function _before_write(&$data)
     {
-        if(IS_POST){
-            $data['active'] = $this->setActive();
+        
+        if (IS_POST) {
+            $data[ 'active' ] = $this->setActive();
         }
         
         parent::_before_write($data);
+    }
+    
+    function setActive()
+    {
+        
+        if (I('post.active') == 'on') {
+            return 1;
+        }
+        
+        return 0;
     }
 }
